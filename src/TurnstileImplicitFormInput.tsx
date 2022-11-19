c89b3c8312e7010b9818432121c819b3a3bb8cef
Hyeseong Kim <hey@hyeseong.kim>
2022-11-19 18:42:14 +0900

    fix token challenge to be using js

--- src/TurnstileImplicitFormInput.tsx
+++ src/TurnstileImplicitFormInput.tsx
@@ -14,10 +14,31 @@ export default function TurnstileImplicitFormInput({
   const siteKey = overrideSiteKey || pluginSiteKey;
   const turnstileReady = useTurnstileReady();
 
+  const [token, setToken] = React.useState<string | null>(null);
+  const ref = React.useRef<HTMLDivElement>(null);
+
+  React.useEffect(() => {
+    if (!(ref.current && turnstileReady)) {
+      return;
+    }
+    const widgetId = window.turnstile.render(ref.current, {
+      siteKey,
+      callback: setToken,
+    });
+    return () => {
+      if (widgetId) {
+        window.turnstile.remove(widgetId);
+      }
+    };
+  }, [siteKey, turnstileReady]);
+
   return (
     <>
       {turnstileReady && (
-        <div className="cf-turnstile" data-sitekey={siteKey} />
+        <div ref={ref} className="cf-turnstile" data-sitekey={siteKey} />
+      )}
+      {token && (
+        <input type="hidden" name="cf-turnstile-response" value={token} />
       )}
       <noscript>
         <div className="cf-turnstile" data-sitekey={siteKey} />
